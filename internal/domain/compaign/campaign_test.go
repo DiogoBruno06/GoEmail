812e15fcb43e5580afca6fd68519fa20442adf3f
DiogoBruno06 <diogobruno06@outlook.com>
2024-08-15 19:51:04 -0300

    feat: Adicionando Testes Especificos e Constante

--- internal/domain/compaign/campaign_test.go
+++ internal/domain/compaign/campaign_test.go
@@ -7,13 +7,15 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var (
+	name     = "Campaign X"
+	content  = "Body"
+	contatcs = []string{"email1@e.com", "email2@e.com"}
+)
+
 func Test_NewCampaign_CreateCampaign(t *testing.T) {
 	assert := assert.New(t)
-	name := "Campaign X"
-	content := "Body"
-	contatcs := []string{"email1@e.com", "email2@e.com"}
-
-	campaign := NewCampaign(name, content, contatcs)
+	campaign, _ := NewCampaign(name, content, contatcs)
 
 	assert.Equal(campaign.Name, name)
 	assert.Equal(campaign.Content, content)
@@ -22,23 +24,38 @@ func Test_NewCampaign_CreateCampaign(t *testing.T) {
 
 func Test_NewCampaignID_IDisNotNull(t *testing.T) {
 	assert := assert.New(t)
-	name := "Campaign X"
-	content := "Body"
-	contatcs := []string{"email1@e.com", "email2@e.com"}
 
-	campaign := NewCampaign(name, content, contatcs)
+	campaign, _ := NewCampaign(name, content, contatcs)
 
 	assert.NotNil(campaign.ID)
 }
 
-func Test_NewCampaignID_CreatedOnisNotNull(t *testing.T) {
+func Test_NewCampaign_CreatedOnMustBeNow(t *testing.T) {
 	assert := assert.New(t)
-	name := "Campaign X"
-	content := "Body"
-	contatcs := []string{"email1@e.com", "email2@e.com"}
 	now := time.Now().Add(-time.Minute)
 
-	campaign := NewCampaign(name, content, contatcs)
+	campaign, _ := NewCampaign(name, content, contatcs)
 
 	assert.Greater(campaign.CreatedOn, now)
+}
+
+func Test_NewCampaign_MustValidateName(t *testing.T) {
+	assert := assert.New(t)
+
+	_, err := NewCampaign("", content, contatcs)
+	assert.Equal("name is required", err.Error())
+}
+
+func Test_NewCampaign_MustValidateContent(t *testing.T) {
+	assert := assert.New(t)
+
+	_, err := NewCampaign(name, "", contatcs)
+	assert.Equal("content is required", err.Error())
+}
+
+func Test_NewCampaign_MustValidateContacts(t *testing.T) {
+	assert := assert.New(t)
+
+	_, err := NewCampaign(name, content, []string{})
+	assert.Equal("content is required", err.Error())
 }
